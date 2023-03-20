bf710adbc325456741300547c8eb8b457ffcc879
Nida Uğur Şişik <44878194+NidaUgurSisik@users.noreply.github.com>
2023-03-20 15:54:10 +0300

    a

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import re
-
+import requests
+from bs4 import BeautifulSoup
 import pandas as pd
 import streamlit as st
 import requests
@@ -7,12 +8,6 @@ from PyPDF2 import PdfReader
 from functionforDownloadButtons import download_button
 from io import StringIO
 
-from pdfminer.converter import TextConverter
-from pdfminer.layout import LAParams
-from pdfminer.pdfdocument import PDFDocument
-from pdfminer.pdfinterp import PDFResourceManager, PDFPageInterpreter
-from pdfminer.pdfpage import PDFPage
-from pdfminer.pdfparser import PDFParser
 
 
 def _max_width_():
@@ -32,32 +27,24 @@ def _max_width_():
 st.set_page_config(page_icon="images/icon.png", page_title="Entity Extractor")
 
 
-API_KEY = st.sidebar.text_input(
-    "Enter your HuggingFace API key",
-    help="Once you created you HuggingFace account, you can get your free API token in your settings page: https://huggingface.co/settings/tokens",
-    type="password",
-)
+url = "https://seeking-alpha.p.rapidapi.com/analysis/v2/list"
 
-# Adding the HuggingFace API inference URL.
-API_URL = "https://api-inference.huggingface.co/models/StanfordAIMI/stanford-deidentifier-base"
+querystring = {"id":keyword,"size":size,"number":"1"}
 
-# Now, let's create a Python dictionary to store the API headers.
-headers = {"Authorization": f"Bearer {API_KEY}"}
+headers = {
+	"X-RapidAPI-Key": "7b530f132bmsh9ce89c66c2eb5d1p1864c8jsnd7c0c3f9ed02",
+	"X-RapidAPI-Host": "seeking-alpha.p.rapidapi.com"
+}
 
-df = pd.DataFrame()
-person = []
-location = []
-date = []
-id = []
-company = []
-output = []
+response = requests.request("GET", url, headers=headers, params=querystring).json()
+
 c2, c3 = st.columns([6, 1])
 
 with c2:
     c31, c32 = st.columns([12, 2])
     with c31:
         st.caption("")
-        st.title("Entity Extractor")
+        st.title("Seeking Alpha")
     with c32:
         st.image(
             "images/logo.png",
@@ -65,43 +52,11 @@ with c2:
         )
 
 
-# Convert PDF to JPG
 
-uploaded_file = st.file_uploader(
-    " ",
-    key="1",
-    help="To activate 'wide mode', go to the hamburger menu > Settings > turn on 'wide mode'",
-)
+def GetArticle():
+    response = requests.request("GET", url, headers=headers, params=querystring).json()
+    return response
 
-
-def copyWriter(payload):
-    response = requests.post(API_URL, headers=headers, json=payload)
-    return response.json()
-
-if uploaded_file is not None:
-    output_string = StringIO()
-    
-    parser = PDFParser(uploaded_file)
-    doc = PDFDocument(parser)
-    rsrcmgr = PDFResourceManager()
-    device = TextConverter(rsrcmgr, output_string, laparams=LAParams())
-    interpreter = PDFPageInterpreter(rsrcmgr, device)
-    for page in PDFPage.create_pages(doc):
-        interpreter.process_page(page)
-    output = copyWriter({
-            "inputs": output_string.getvalue(),
-        })
-
-    #st.write(merged_text)
-    st.subheader('Your text before processing.')
-    st.write(output_string.getvalue())
-    result_text = output_string.getvalue()
-mask_char = '*'
-for i in output:
-    #output_string[i['start'], i['end']]
-    result_text = result_text[:i['start']] + mask_char*(i['end']-i['start']) + result_text[i['end']:]
-
-st.write(result_text)
-st.subheader('Your text after processing.')
-last_result = re.sub(r'\*{5,}', '*****', result_text)
-st.write(last_result)
+with c3:
+    Stock = st.text_input('Stock Name', '')
+    st.write('The current Stock Name is', Stock)
